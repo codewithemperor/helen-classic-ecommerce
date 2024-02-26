f6d24d305e684e66b688ec70b7d7afdb26f719eb
Sulaimon Yusuf Ayomide <codewithemperor@gmail.com>
2024-02-26 14:38:37 +0100

    modified:   assets/js/script.js 	modified:   index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,12 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > window.innerHeight || document.documentElement.scrollTop > window.innerHeight) {
-    document.getElementById("navbar").classList.add("blur");
-    console.log(innerHeight);
-    console.log(document.body.scrollTop);
-    console.log(document.documentElement.scrollTop);
+    document.getElementById("navbar").classList.remove("sticky-top");
+    document.getElementById("navbar").classList.add("fixed-top");
+
   } else {
-    document.getElementById("navbar").classList.remove("blur");
-    console.log(innerHeight);
-    console.log(document.body.scrollTop);
-    console.log(document.documentElement.scrollTop);
+    document.getElementById("navbar").classList.add("sticky-top");
+    document.getElementById("navbar").classList.remove("fixed-top");
+
   }
 }

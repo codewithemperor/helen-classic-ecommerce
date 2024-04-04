d8f84c87ff8e8836b3d0449f7a931efaeff70250
Sulaimon Yusuf <codewithemperor@gmail.com>
2024-04-04 18:15:42 +0100

    modified:   assets/css/style.css 	modified:   assets/js/script.js 	modified:   index.html 	modified:   product.html 	new file:   products.json

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,3 +33,45 @@ const colorOptions = document.querySelectorAll('.color-option');
     colorOptions.forEach(option => {
         option.addEventListener('click', selectColor);
     });
+
+// json
+function fetchProducts() {
+  return fetch('../products.json') // Adjusted path to access products.json
+      .then(response => response.json())
+      .catch(error => console.error('Error fetching products:', error));
+}
+
+
+function renderProducts(products) {
+  const productListDiv = document.getElementById('product-list');
+  if (!productListDiv) {
+      console.error('Product list container not found!');
+      return;
+  }
+
+  products.forEach(product => {
+      const productDiv = document.createElement('div');
+      productDiv.innerHTML = `
+          <div class="col gap-3">
+              <a href="product.html" class="product" id="product${product.id}">
+                  <div class="product-image">
+                      <img src="${product.image}" class="product-img" alt="...">
+                  </div>
+                  <div class="product-description">
+                      <p class="product-description__name text-truncate">${product.name}</p>
+                      <h6 class="product-description__price"><span class="naira">N</span>${product.price.toFixed(2)}</h6>
+                      <p class="product-description__fprice m-0">${product.discounted_price.toFixed(2)}</p>
+                  </div>
+              </a>
+          </div>
+      `;
+      productListDiv.appendChild(productDiv);
+  });
+}
+
+// Load products when the page is loaded
+window.onload = function() {
+  fetchProducts().then(products => {
+      renderProducts(products);
+  });
+};
